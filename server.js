3032d043ffcc6dd768063257814c13e3e6fe9663
Abhishek Harish <itsjustabhii@gmail.com>
2022-07-21 20:32:46 +0530

    initial commit of auth server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { createUserTokensTable } = require('./dao/repository/userToken.repository');
+const authRouter = require('./routes/authRouter')
+const app = express();
+app.use(express.json());
+app.use('/auth', authRouter);
+
+(() => {
+    createUserTokensTable();
+})();
+
+app.listen(4000, (err) => {
+    if(err) {
+        console.log('Error in starting server', err);
+    }
+})
+
+/*Auth Server and Ecommerce are two different applications deployed separately on two different servers */
